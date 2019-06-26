445ff8cdfc71cdecc367a7857ec6eca1e2312068
Ko Takagi <knockoutmarch@gmail.com>
2019-06-26 23:33:09 +0900

    :wrench: first commit

---
+++ youtube-explorer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Youtube Explorer
+ * Plugin URI:      https://github.com/ko31/youtube-explorer
+ * Description:     Do you want me to help you search YouTube?
+ * Author:          ko31
+ * Author URI:      https://go-sign.info
+ * Text Domain:     youtube-explorer
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Youtube_Explorer
+ */
+
+// Your code starts here.
